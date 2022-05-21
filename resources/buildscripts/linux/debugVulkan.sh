e6b910ab05c6323ac0e0f370f3289149031bb57c
NoamZeise <noam4134@gmail.com>
2022-05-21 01:28:20 +0100

    added build scripts for linux

--- resources/buildscripts/linux/debugVulkan.sh
+++ resources/buildscripts/linux/debugVulkan.sh
@@ -1,10 +1,10 @@
 cd build
-cmake .. -G"Ninja Multi-Config" -DCMAKE_EXPORT_COMPILE_COMMANDS=1
+cmake .. -G"Ninja Multi-Config" -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DGFX_ENV_VULKAN=1
 cmake --build . --config Debug
 cd ..
 cp -r resources/audio build/Debug
 cp -r resources/shaders build/Debug
-cp -r resources/models build/Debug
+cp -r resources/maps build/Debug
 cp -r resources/textures build/Debug
 cd build/Debug
 ./Vulkan-Environment
